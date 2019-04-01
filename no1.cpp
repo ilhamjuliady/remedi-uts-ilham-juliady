468fe79e4be2fa6debd6cb2cb66de512d4644bbd
ilhamjuliady <49119247+ilhamjuliady@users.noreply.github.com>
2019-04-01 19:43:00 +0700

    remedial no 1
    memeperbaiki no 1 walaupun nilainya sudah mencukupi,hehe

---
+++ no1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout<<"Nama : Ajis Gagap"<<endl;
+  cout<<"Ips semester 1 : 3.0"<<endl;  
+  cout<<"Ipk : 2.5"<<endl;
+
+
+return 0;
+}
