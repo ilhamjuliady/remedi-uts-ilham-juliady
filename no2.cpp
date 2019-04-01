6fe2db8518faf0304f3d8be10384c8f0503e58b0
ilhamjuliady <49119247+ilhamjuliady@users.noreply.github.com>
2019-04-01 20:14:11 +0700

    remedialno2.cpp
    remedial no2 saya mendapatkan nilai 8 sebelumnya

---
+++ no2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namesapce std;
+
+struct data_mhs
+{
+  string Nama;
+  float Nim ips;
+  float ipk;
+} a,b;
+
+int main()
+{
+  a.nama = "Ajis Gagap";
+  a.ips 1  = 3.0;
+  a.ipk = 2.5;
+  
+  b.nama = "Tumming";
+  b.ips 1 = 3.1;
+  b.ipk = 3.0;
+  
+  cout<<"Nama : "<< a.nama <<endl;
+  cout<<"Ips semester 1" << a.ips 1 <<endl;
+  cout<<"Ipk : <<a.Ipk <<endl;
+  cout<<"\n";
+  cout<< "Nama : "<< b.nama<<endl;
+  cout<< "Ips semester 1"<<b.ips 1<<endl;
+  cout<< "Ipk :"<< b.ipk <<endl;
+  
+ return 0;
+}
